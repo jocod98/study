c1a903b97b2f86e52d2aec8bf2ce640db0ef101b
jocod98 <106361304+jocod98@users.noreply.github.com>
2023-05-22 21:27:12 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+view = [100,150,30]
+st.write("# Youtube View")
+st.write("## raw")
+view
+st.write("## Bar Chart")
+st.bar_chart(view)
+sview= pd.Series(view)
+sview
